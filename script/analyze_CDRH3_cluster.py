011248d1873d1f5c91809076e498fd5c6395ab10
Nicholas Wu <wchnicholas@wirelessprv-10-194-247-108.near.illinois.edu>
2023-02-03 14:52:27 -0600

    first commit

---
+++ script/analyze_CDRH3_cluster.py
@@ -0,0 +1,160 @@
+#!/usr/bin/python
+import os
+import sys
+import glob
+import pandas as pd
+from collections import defaultdict, Counter
+
+def reading_cluster_info(file_cluster):
+  infile = open(file_cluster, 'r')
+  cluster_dict = defaultdict(list)
+  for line in infile.readlines():
+    if 'cluster' in line: continue
+    ID, CDRH3, cluster = line.rstrip().rsplit("\t")
+    cluster_dict[cluster].append(ID)
+  infile.close()
+  return cluster_dict
+
+def cleanup_cluster(cluster_dict):
+  for n in sorted(cluster_dict.keys()):
+    donors = list(set([ab.rsplit('|')[0] for ab in cluster_dict[n]]))
+    if len(donors) == 1:
+      del cluster_dict[n]
+    elif len(donors) == 2 and 'VRC_310_cohort' in donors and ''.join(donors).count('VRC_310_cohort') == 2:
+      del cluster_dict[n]
+    elif len(donors) == 2 and 'Harrison_donors' in donors and ''.join(donors).count('Harrison_donor') == 2:
+      del cluster_dict[n]
+  return cluster_dict
+
+def reading_Ab_table(filename):
+  print ('reading: %s' % filename)
+  datasheet = pd.read_excel(filename, sheet_name='Sheet1')
+  Ab_dict = {}
+  for index, info_dict in datasheet.iterrows():
+    if str(info_dict['Donor ID']) == '': continue
+    if str(info_dict['CDRH3_AA']) == '': continue
+    ID = str(info_dict['Donor ID'])+'|'+str(info_dict['Name'])
+    for i in info_dict.keys():
+      info_dict[i] = str(info_dict[i])
+    Ab_dict[ID] = info_dict
+  return Ab_dict, list(datasheet.columns)
+
+def classify_epitope(epitopes):
+  epi_list = []
+  for epitope in epitopes:
+    if epitope in ['NP', 'NA:Unk', 'Other'] or epitope == '':
+      continue
+    elif epitope in ['HA:Unk', 'H1 HA', 'Multiple', 'Unknown', 'HA or NP or M1']:
+      epi_list.append('HA:Stem')
+      epi_list.append('HA:Head')
+    elif epitope in ['HA:Stem', 'HA:Stem, CR9114-competing', 'HA:Stem, non-CR9114-competing']:
+      epi_list.append('HA:Stem')
+    elif epitope == 'HA:Head':
+      epi_list.append('HA:Head')
+    else:
+      print (epitope)
+  if len(epi_list) == 0:
+    return ('unknown')
+  max_epi = max(epi_list,key=epi_list.count)
+  if epi_list.count(max_epi)/float(len(epi_list)) > 0.5:
+    return (max_epi)
+  else:
+    return ('unknown')
+
+def merge_Vgene(Vgene):
+  if 'IGHV4-38' in Vgene: 
+    return (Vgene)
+  if 'IGHV5-10' in Vgene: 
+    return (Vgene)
+  if Vgene == 'IGHV3-53' or Vgene == 'IGHV3-66':
+    return 'IGHV3-53/3-66'
+  if Vgene == 'IGHV4-30-2' or Vgene == 'IGHV4-30-4':
+    return 'IGHV4-30-2/4-30-4'
+  if Vgene == 'IGHV3-30-3':
+    return 'IGHV3-30'
+  if Vgene.count('-') == 2:
+    adj_Vgene = '-'.join(Vgene.rsplit('-')[0:2])
+    print ('converting: %s to %s' % (Vgene, adj_Vgene))
+    return adj_Vgene
+  else:
+    return Vgene
+
+def find_top_Vgene(Vgenes):
+  Vgenes = list(map(merge_Vgene, [Vgene.rsplit(',')[0].rsplit('*')[0] for Vgene in Vgenes]))
+  top_Vgene = max(Vgenes,key=Vgenes.count)
+  freq      = Vgenes.count(top_Vgene)/float(len(Vgenes))
+  if len(set(Vgenes))==1 and Vgenes[0]=='nan':
+    return 'nan', 'nan'
+  elif freq <= 0.5:
+    return 'Diverse', freq
+  else:
+    return top_Vgene, freq
+
+def extract_Vgene_info(HC_Vgenes, LC_Vgenes):
+  top_HV, top_HV_freq = find_top_Vgene(HC_Vgenes)
+  top_LV, top_LV_freq = find_top_Vgene(LC_Vgenes)
+  return top_HV, top_HV_freq, top_LV, top_LV_freq
+
+def seqs2consensus(seqlist):
+  consensus = ''
+  for n in range(len(seqlist[0])):
+    resi = []
+    for seq in seqlist:
+      resi.append(seq[n])
+    most_common,num_most_common = Counter(resi).most_common(1)[0]
+    consensus+=most_common
+  return consensus
+
+def write_cluster_info(outfile1, outfile2, header, Ab_dict, cluster_dict):
+  print ('writing: %s' % outfile1)
+  print ('writing: %s' % outfile2)
+  outfile1 = open(outfile1, 'w')
+  outfile2 = open(outfile2, 'w')
+  outfile1.write('cluster'+"\t"+"\t".join(header)+"\n")
+  outfile2.write("\t".join(['cluster_ID', 'cluster_size', 'num_donors', 'epitope', 
+                            'top_HV', 'top_LV', 'top_HV_freq', 'top_LV_freq', 'CDRH3_consen', 'donors', 'ab_names'])+"\n")
+  cluster_ID = 0
+  for n in sorted(cluster_dict.keys(), key=lambda x:len(cluster_dict[x]), reverse=True):
+    if str(Ab_dict[cluster_dict[n][0]]['CDRH3_AA']) in ['NA', 'nan']:
+      print ("One cluster with 'NA' CDRH3 is removed")
+      continue
+    cluster_ID += 1
+    HC_Vgenes = []
+    LC_Vgenes = []
+    CDRH3s    = []
+    donor_names  = []
+    ab_names  = []
+    for Ab in cluster_dict[n]:
+      outfile1.write(str(cluster_ID)+"\t"+"\t".join([str(Ab_dict[Ab][str(item)]) for item in header])+"\n")
+      HC_Vgenes.append(str(Ab_dict[Ab]['Heavy_V_gene']))
+      LC_Vgenes.append(str(Ab_dict[Ab]['Light_V_gene']))
+      CDRH3s.append(str(Ab_dict[Ab]['CDRH3_AA']))
+      donor_names.append(Ab.rsplit('|')[0])
+      ab_names.append(Ab.rsplit('|')[1])
+    CDRH3_consen = seqs2consensus(CDRH3s)
+    top_HV, top_HV_freq, top_LV, top_LV_freq = extract_Vgene_info(HC_Vgenes, LC_Vgenes)
+    cluster_size = len(cluster_dict[n])
+    donors       = len(set([Ab.rsplit('|')[0] for Ab in cluster_dict[n]]))
+    epitopes     = [Ab_dict[Ab]['Binds to'] for Ab in cluster_dict[n]]
+    epitope      = classify_epitope(epitopes)
+    outfile2.write("\t".join(map(str, [cluster_ID, cluster_size, donors, epitope,
+                                       top_HV, top_LV, top_HV_freq, top_LV_freq, CDRH3_consen,
+                                       ','.join(sorted(list(set(donor_names)))),
+                                       ','.join(sorted(list(set(ab_names))))]))+"\n")
+  outfile1.close()
+  outfile2.close()
+
+def main():
+  outfile1  = 'result/Ab_info_CDRH3_clustering.tsv'
+  outfile2  = 'result/CDRH3_cluster_summary.tsv'
+  filename = 'doc/HA_Abs_v14.10.xlsx'
+  file_cluster  = 'result/CDRH3_cluster.tsv'
+  Ab_dict, header  = reading_Ab_table(filename)
+  cluster_dict  = reading_cluster_info(file_cluster)
+  print ("total number of clusters: %i" % len(cluster_dict.keys()))
+  cluster_dict  = cleanup_cluster(cluster_dict)
+  print ("total number of clusters after clean up: %i" % len(cluster_dict.keys()))
+  write_cluster_info(outfile1, outfile2, header, Ab_dict, cluster_dict)
+
+if __name__ == "__main__":
+  main()
---
+++ script/analyze_CDRH3_property.py
@@ -0,0 +1,198 @@
+#!/usr/bin/python
+import os
+import sys
+import glob
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+from Bio.Seq import Seq
+from collections import defaultdict, Counter
+
+def read_ab_info(filename):
+  aas = ["A", "R", "N", "D", "C", "E", "Q", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "W", "Y", "V"]
+  print ('reading: %s' % filename)
+  datasheet = pd.read_excel(filename, sheet_name='Sheet1')
+  Ab_dict = {}
+  for index, info_dict in datasheet.iterrows():
+    for i in info_dict.keys():
+      info_dict[i] = str(info_dict[i])
+    Ab_dict[info_dict['Name']] = info_dict
+  return Ab_dict
+
+def read_clonoinfo(filename):
+  print ('reading: %s' % filename)
+  df = pd.read_excel(filename, sheet_name="Sheet1")
+  df = df[df.clonotype.notnull()]
+  clono_dict = {}
+  for index, info_dict in df.iterrows():
+    for i in info_dict.keys():
+      info_dict[i] = str(info_dict[i])
+    clono_dict[info_dict['Name']] = info_dict['clonotype']
+  return clono_dict
+
+def classify_epitope(epitopes):
+  epi_list = []
+  for epitope in epitopes:
+    if epitope in ['NP', 'NA:Unk', 'Other'] or epitope == '':
+      continue
+    elif epitope in ['HA:Unk']:
+      epi_list.append('HA:Stem')
+      epi_list.append('HA:Head')
+    elif epitope in ['HA:Stem']:
+      epi_list.append('HA:Stem')
+    elif epitope == 'HA:Head':
+      epi_list.append('HA:Head')
+    else:
+      print (epitope)
+  if len(epi_list) == 0:
+    return ('unknown')
+  max_epi = max(epi_list,key=epi_list.count)
+  if epi_list.count(max_epi)/float(len(epi_list)) > 0.5:
+    return (max_epi)
+  else:
+    return ('unknown')
+
+def seqs2consensus(seqlist, most_common_len):
+  consensus = ''
+  for n in range(most_common_len):
+    resi = []
+    for seq in seqlist:
+      if len(seq) == most_common_len:
+        resi.append(seq[n])
+    most_common,num_most_common = Counter(resi).most_common(1)[0]
+    consensus+=most_common
+  return consensus
+
+def generate_CDR_concensus(CDR_dict):
+  for clono_ID in CDR_dict.keys():
+    CDR_lens = [len(CDR) for CDR in CDR_dict[clono_ID]]
+    most_common_len, num_most_common_len = Counter(CDR_lens).most_common(1)[0]
+    CDR_dict[clono_ID] = seqs2consensus(CDR_dict[clono_ID], most_common_len)
+  return CDR_dict
+
+def compress_ab_list_CDRH3(Ab_dict, clono_dict):
+  CDRH1_dict = defaultdict(list)
+  CDRH2_dict = defaultdict(list)
+  CDRH3_dict = defaultdict(list)
+  CDRL1_dict = defaultdict(list)
+  CDRL2_dict = defaultdict(list)
+  CDRL3_dict = defaultdict(list)
+  VH_dict    = defaultdict(list)
+  VL_dict    = defaultdict(list)
+  epi_dict = defaultdict(list)
+  for Ab in Ab_dict.keys():
+    CDRH1 = Ab_dict[Ab]['CDRH1_AA']
+    CDRH2 = Ab_dict[Ab]['CDRH2_AA']
+    CDRH3 = Ab_dict[Ab]['CDRH3_AA']
+    CDRL1 = Ab_dict[Ab]['CDRL1_AA']
+    CDRL2 = Ab_dict[Ab]['CDRL2_AA']
+    CDRL3 = Ab_dict[Ab]['CDRL3_AA']
+    VH    = Ab_dict[Ab]['VH_AA']
+    VL    = Ab_dict[Ab]['VL_AA']
+    donor = Ab_dict[Ab]['Donor ID']
+    clono = clono_dict[Ab] if Ab in clono_dict.keys() else 'none'
+    clono_ID = Ab if clono=='none' else clono+'|'+donor 
+    epi      = Ab_dict[Ab]['Binds to']
+    epi_dict[clono_ID].append(epi)
+    if CDRH1 != 'nan': CDRH1_dict[clono_ID].append(CDRH1)
+    if CDRH2 != 'nan': CDRH2_dict[clono_ID].append(CDRH2)
+    if CDRH3 != 'nan': CDRH3_dict[clono_ID].append(CDRH3)
+    if CDRL1 != 'nan': CDRL1_dict[clono_ID].append(CDRL1)
+    if CDRL2 != 'nan': CDRL2_dict[clono_ID].append(CDRL2)
+    if CDRL3 != 'nan': CDRL3_dict[clono_ID].append(CDRL3)
+    if VH != 'nan': VH_dict[clono_ID].append(VH)
+    if VL != 'nan': VL_dict[clono_ID].append(VL)
+  for clono_ID in epi_dict.keys():
+    epi_dict[clono_ID]  = classify_epitope(epi_dict[clono_ID])
+  CDRH1_dict = generate_CDR_concensus(CDRH1_dict)
+  CDRH2_dict = generate_CDR_concensus(CDRH2_dict)
+  CDRH3_dict = generate_CDR_concensus(CDRH3_dict)
+  CDRL1_dict = generate_CDR_concensus(CDRL1_dict)
+  CDRL2_dict = generate_CDR_concensus(CDRL2_dict)
+  CDRL3_dict = generate_CDR_concensus(CDRL3_dict)
+  VH_dict = generate_CDR_concensus(VH_dict)
+  VL_dict = generate_CDR_concensus(VL_dict)
+  return CDRH1_dict, CDRH2_dict, CDRH3_dict, CDRL1_dict, CDRL2_dict, CDRL3_dict, VH_dict, VL_dict, epi_dict
+
+def add_GB_ab(CDRH3_dict, epi_dict, GB_dict):
+  for Ab in GB_dict.keys():
+    CDRH3 = GB_dict[Ab]['CDRH3_AA']
+    if CDRH3 != 'nan': CDRH3_dict[Ab] = CDRH3
+    epi_dict[Ab] = 'GenBank'
+  return CDRH3_dict, epi_dict
+
+def analyze_CDRH3_property(CDRH3_dict, epi_dict, outfile):
+  print ('writing: %s' % outfile)
+  outfile = open(outfile, 'w')
+  outfile.write("\t".join(['clone', 'epi', 'CDRH3', 'CDRH3_tip', 'len_CDRH3',
+                           'CDRH3_H_score', 'CDRH3_C_score', 'CDRH3_A_score',
+                           'tip_H_score', 'tip_C_score', 'tip_A_score'])+"\n")
+  H_scale = {'A':0.17,'R':0.81,'N':0.42,'D':1.23,'C':-0.24,
+             'Q':0.58,'E':2.02,'G':0.01,'H':0.96,'I':-0.31,
+             'L':-0.56,'K':0.99,'M':-0.23,'F':-1.13,'P':0.45,
+             'S':0.13,'T':0.14,'W':-1.85,'Y':-0.94,'V':0.07}
+  C_scale = {'A':0,'R':1,'N':0,'D':-1,'C':0,
+             'Q':0,'E':-1,'G':0,'H':0.25,'I':0,
+             'L':0,'K':1,'M':0,'F':0,'P':0,
+             'S':0,'T':0,'W':0,'Y':0,'V':0}
+  A_scale = {'A':0,'R':0,'N':0,'D':0,'C':0,
+             'Q':0,'E':0,'G':0,'H':0,'I':0,
+             'L':0,'K':0,'M':0,'F':1,'P':0,
+             'S':0,'T':0,'W':1,'Y':1,'V':0}
+  for clono_ID in CDRH3_dict.keys():
+    CDRH3 = CDRH3_dict[clono_ID]
+    epi   = epi_dict[clono_ID].replace('HA:','')
+    if set(list(CDRH3)).issubset(H_scale.keys()):
+      if len(CDRH3)%2==0:
+        CDRH3_tip = CDRH3[int(len(CDRH3)/2)-2:int(len(CDRH3)/2)+2]
+      else:
+        CDRH3_tip = CDRH3[int(len(CDRH3)/2)-1:int(len(CDRH3)/2)+2]
+      tip_H_score = -sum([H_scale[aa] for aa in CDRH3_tip])/float(len(CDRH3_tip))*10
+      tip_C_score = sum([abs(C_scale[aa]) for aa in CDRH3_tip])/float(len(CDRH3_tip))
+      tip_A_score = sum([A_scale[aa] for aa in CDRH3_tip])/float(len(CDRH3_tip))
+      CDRH3_H_score = -sum([H_scale[aa] for aa in CDRH3])/float(len(CDRH3))*10
+      CDRH3_C_score = sum([abs(C_scale[aa]) for aa in CDRH3])/float(len(CDRH3))
+      CDRH3_A_score = sum([A_scale[aa] for aa in CDRH3])/float(len(CDRH3))
+    outfile.write("\t".join(map(str, [clono_ID, epi, CDRH3, CDRH3_tip, len(CDRH3),
+                  CDRH3_H_score, CDRH3_C_score, CDRH3_A_score,
+                  tip_H_score, tip_C_score, tip_A_score]))+"\n")
+  outfile.close()
+
+def write_CDRs(CDRH1_dict, CDRH2_dict, CDRH3_dict, CDRL1_dict, CDRL2_dict, CDRL3_dict, VH_dict, VL_dict, epi_dict, outfile):
+  print ('writing: %s' % outfile)
+  outfile = open(outfile, 'w')
+  clones = set(CDRH1_dict.keys()).intersection(set(CDRH2_dict.keys())).intersection(set(CDRH3_dict.keys())) \
+           .intersection(set(CDRL1_dict.keys())).intersection(set(CDRL2_dict.keys())).intersection(set(CDRL3_dict.keys())) \
+           .intersection(set(VH_dict)).intersection(VL_dict)
+  outfile.write("\t".join(['Name', 'Binds to', 'VH_AA', 'VL_AA',
+                           'CDRH1_AA', 'CDRH2_AA', 'CDRH3_AA', 'CDRL1_AA', 'CDRL2_AA', 'CDRL3_AA'])+"\n")
+  for clone in sorted(clones, key=lambda x:epi_dict[x]):
+    epi   = epi_dict[clone]
+    VH_AA = VH_dict[clone]
+    VL_AA = VL_dict[clone]
+    CDRH1 = CDRH1_dict[clone]
+    CDRH2 = CDRH2_dict[clone]
+    CDRH3 = CDRH3_dict[clone]
+    CDRL1 = CDRL1_dict[clone]
+    CDRL2 = CDRL2_dict[clone]
+    CDRL3 = CDRL3_dict[clone]
+    if epi in ['HA:Stem', 'HA:Head']:
+      outfile.write("\t".join([clone, epi, VH_AA, VL_AA, CDRH1, CDRH2, CDRH3, CDRL1, CDRL2, CDRL3])+"\n")
+  outfile.close()
+
+def main():
+  filename       = 'doc/HA_Abs_v14.10.xlsx'
+  outfile1       = 'result/CDRH3_property.tsv'
+  outfile2       = 'result/Ab_for_model.tsv'
+  file_clonotype = 'result/HA_Abs_clonotype.xlsx'
+  file_GB        = 'doc/all_paired_antibodies_from_GB_v6.xlsx'
+  Ab_dict        = read_ab_info(filename)
+  GB_dict        = read_ab_info(file_GB)
+  clono_dict     = read_clonoinfo(file_clonotype)
+  CDRH1_dict,CDRH2_dict,CDRH3_dict,CDRL1_dict,CDRL2_dict,CDRL3_dict,VH_dict,VL_dict,epi_dict = compress_ab_list_CDRH3(Ab_dict, clono_dict)
+  CDRH3_dict, epi_dict = add_GB_ab(CDRH3_dict, epi_dict, GB_dict)
+  analyze_CDRH3_property(CDRH3_dict, epi_dict, outfile1)
+  write_CDRs(CDRH1_dict, CDRH2_dict, CDRH3_dict, CDRL1_dict, CDRL2_dict, CDRL3_dict, VH_dict, VL_dict, epi_dict, outfile2)
+
+if __name__ == "__main__":
+  main()
